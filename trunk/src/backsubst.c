d6163a494ea8e8c1abb9305befa220827103d179
Kacper7602 <01187602@pw.edu.pl>
2023-12-22 08:30:46 +0100

    implemtancja podstawiania wstecznego

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -1,22 +1,29 @@
 #include "backsubst.h"
+
 /**
- * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
+ * Zwraca 0 - wsteczne podstawienie zakończone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    if (mat->r != mat->c || mat->r != b->r || x->r != b->r || x->c != 1) {
+        printf("Nieprawidlowy rozmiar macierzy\n");
+        return 2;
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    int n = mat->r;
+    for (int i = n - 1; i >= 0; --i) {
+        if (mat->data[i][i] == 0) {
+            printf("Element na diagonali jest równy 0\n");
+            return 1;
+        }
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+        double sum = 0;
+        for (int j = i + 1; j < n; ++j) {
+            sum += mat->data[i][j] * x->data[j][0];
+        }
+        x->data[i][0] = (b->data[i][0] - sum) / mat->data[i][i];
+    }
 
-				return 0;
+    return 0;
 }
-
-
